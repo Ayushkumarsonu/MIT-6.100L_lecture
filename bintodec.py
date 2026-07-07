5fccd71265d5bd973ea8a193afca456dfdaba882
Ayushkumarsonu <ayushsonu398@gmail.com>
2026-07-07 17:49:05 +0530

    Binary To Decimal Number

--- bintodec.py
+++ bintodec.py
@@ -1,10 +1,6 @@
-num = int(input("Enter any decimal number"))  # asks user input
-if num == 0 :
-    result =0                           # the binary form of zero is zero 
-    print("It's binary form is " , result)
-else :
-    result = ''
-    while num > 0 :    # loops runs until the user input is positive
-        result = str(num%2) + result       # the remainder of the user input divided by 2 is concatenated to the left of the result variable which is initialized as an empty string
-        num = num//2                    # divides the number by 2 and assigns the quotient to the variable num 
-    print(f"The binary form of the decimal number is {result}") # print
+binary = int(input("Enter any binary number"))  # asks user input 
+mysum=0
+digits = [int(d) for d in str(binary)]          # first converts the binary number to string an then converts each digit to integer and stores it in a list called digits
+for i, d in enumerate(digits):                  # enumerate function is used to get the index and the value of each digit in the list digits
+    mysum += d * (2 ** (len(digits) - 1 - i))   # the value of each digit is multiplied by 2 raised to the power of its index in the list digits and added to mysum
+print(f"The decimal value of the binary number is {mysum}")  # print
